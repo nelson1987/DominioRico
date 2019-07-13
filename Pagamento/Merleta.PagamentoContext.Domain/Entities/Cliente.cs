fcb1d7b886f154af758cfee6dfc53c4e2d80f535
Nelson Neto <nelson.ash@outlook.com>
2019-07-13 09:34:30 -0300

    Create Cliente

---
+++ Pagamento/Merleta.PagamentoContext.Domain/Entities/Cliente.cs
@@ -0,0 +1,8 @@
+﻿namespace Merleta.PagamentoContext.Domain.Entities
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Documento { get; private set; }
+    }
+}
